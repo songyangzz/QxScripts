a4dcdd8b006e5577cb73b7beb7e6ee96395e53a7
sy <623984375@qq.com>
2020-02-09 21:31:58 +0800

    11

--- sazs34/task/weather_week.js
+++ sazs34/task/weather_week.js
@@ -6,7 +6,7 @@ let config = {
     darksky_api: "3c80d934cb39f805ff3455a90404adbe", //从https://darksky.net/dev/ 上申请key填入即可
     aqicn_api: "9daa8632c28ad0060457c62b6b610ec2382fc6d9", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
     huweather_apiKey: "b3333a5a52fe4fdbbbae544b223951a3", //和风天气APIkey,可自行前往 https://dev.heweather.com/ 进行获取
-    lat_lon: "39.7395084, 116.3345957", //请填写经纬度,直接从谷歌地图中获取即可
+    lat_lon:"39.738476925343484,116.32787581844295", //请填写经纬度,直接从谷歌地图中获取即可
     lang: "zh", //语言,随意切换为您想要的语言哦(zh/zh-tw/ja/en/fr/...)
     log: 0, //调试日志,0为不开启,1为开启,2为开启精简日志
     timeout: 0, //超时时间,单位毫秒(1000毫秒=1秒),一般不推荐修改[为0则不限制超时时间]
