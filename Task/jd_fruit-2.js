6d21dd79d8e27278407643df3d1f11ac7fb26c3d
songyang <623984375@qq.com>
2020-08-12 08:20:51 +0800

    1

--- Task/jd_fruit-2.js
+++ Task/jd_fruit-2.js
@@ -27,7 +27,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 let jdNotify = $.getdata('jdFruitNotify');
 //助力好友分享码(最多4个,否则后面的助力失败),原因:京东农场每人每天只有四次助力机会
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
-  'a6f686a9f6aa4c80977370b03681c553',
+  'f3cfbe650ee64eaf8d1aa985cba35ed5',
   'f92cb56c6a1349f5a35f0372aa041ea0',
   '6fbd26cc27ac44d6a7fed34092453f77',
   '61ff5c624949454aa88561f2cd721bf6',
